aaa5a87ea828ee155db206c280e5ab5614919423
Robert Tkach <119821797+roberttkach@users.noreply.github.com>
2025-09-23 01:30:53 +0900

    Remove unused dedupe helper

--- domain/value/ids.py
+++ domain/value/ids.py
@@ -1,18 +1,5 @@
 from collections.abc import Iterable
 
 
-def dedupe(ids: Iterable[int], sort: bool = False) -> list[int]:
-    if sort:
-        return order(ids)
-    seen = set()
-    out = []
-    for x in ids or []:
-        i = int(x)
-        if i not in seen:
-            seen.add(i)
-            out.append(i)
-    return out
-
-
 def order(ids: Iterable[int]) -> list[int]:
     return sorted({int(x) for x in ids or []})
