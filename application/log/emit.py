a93f718baba162f81df5351a1a00b928c9aba065
Robert Tkach <119821797+roberttkach@users.noreply.github.com>
2025-09-24 10:44:41 +0900

    Simplify log emit exports

--- application/log/emit.py
+++ application/log/emit.py
@@ -1,22 +1,4 @@
-import logging
-
 from ...domain.log.emit import calibrate, jlog
 
 
-def debug(logger, code, **fields):
-    jlog(logger, logging.DEBUG, code, **fields)
-
-
-def info(logger, code, **fields):
-    jlog(logger, logging.INFO, code, **fields)
-
-
-def warning(logger, code, **fields):
-    jlog(logger, logging.WARNING, code, **fields)
-
-
-def error(logger, code, **fields):
-    jlog(logger, logging.ERROR, code, **fields)
-
-
-__all__ = ["jlog", "debug", "info", "warning", "error", "calibrate"]
+__all__ = ["jlog", "calibrate"]
